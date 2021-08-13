b547930e8a49a41771cc23f54f195844ccc481ea
Scott Parker <scottparkerdesign@gmail.com>
2021-08-13 09:38:23 -0400

    move cardInfo into separate js file, add type module to stories.js script tag

---
+++ js/cardInfo.js
@@ -0,0 +1,26 @@
+export const cardInfo = [
+  {
+    date: 'April 16th 2020',
+    title: 'The Mountains',
+    author: 'John Appleseed',
+    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356313/assets/stories/mobile/mountainsDark_kwj25m.jpg',
+  },
+  {
+    date: 'April 1th 2020',
+    title: 'Sunset Cityscapes',
+    author: 'Benjamin Cruz',
+    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356415/assets/stories/mobile/cityscapesDark_muxx6h.jpg',
+  },
+  {
+    date: 'April 11th 2020',
+    title: '18 Days Voyage',
+    author: 'Alexei Borodin',
+    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356510/assets/stories/mobile/voyageDark_ndd55e.jpg',
+  },
+  {
+    date: 'April 9th 2020',
+    title: 'Architecturals',
+    author: 'Samantha Brooke',
+    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356585/assets/stories/mobile/archDark_tbbmpf.jpg',
+  },
+];
--- js/stories.js
+++ js/stories.js
@@ -1,31 +1,6 @@
-const grid = document.querySelector('.storiesPageGrid');
+import { cardInfo } from './cardInfo.js';
 
-const cardInfo = [
-  {
-    date: 'April 16th 2020',
-    title: 'The Mountains',
-    author: 'John Appleseed',
-    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356313/assets/stories/mobile/mountainsDark_kwj25m.jpg',
-  },
-  {
-    date: 'April 1th 2020',
-    title: 'Sunset Cityscapes',
-    author: 'Benjamin Cruz',
-    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356415/assets/stories/mobile/cityscapesDark_muxx6h.jpg',
-  },
-  {
-    date: 'April 11th 2020',
-    title: '18 Days Voyage',
-    author: 'Alexei Borodin',
-    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356510/assets/stories/mobile/voyageDark_ndd55e.jpg',
-  },
-  {
-    date: 'April 9th 2020',
-    title: 'Architecturals',
-    author: 'Samantha Brooke',
-    href: 'https://res.cloudinary.com/djuu2phoy/image/upload/v1628356585/assets/stories/mobile/archDark_tbbmpf.jpg',
-  },
-];
+const grid = document.querySelector('.storiesPageGrid');
 
 const createCard = (card) => `
   <div class="storiesGridCard" style="background-image: url(${card.href});" >
