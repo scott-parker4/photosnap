f736db6a398f8212f426c94ccfcc8090e8b3ad0a
Scott Parker <scottparkerdesign@gmail.com>
2021-08-14 09:56:09 -0400

    edit stories.js to allow for responsive background image switching

--- js/stories.js
+++ js/stories.js
@@ -3,7 +3,17 @@ import { cardInfo } from './cardInfo.js';
 const grid = document.querySelector('.storiesPageGrid');
 
 const createCard = (card) => `
-  <div class="storiesGridCard" style="background-image: url(${card.href});" >
+    <style>
+    .storiesGridCard.card${cardInfo.indexOf(card)} {
+        background-image: url(${card.href});
+    }
+    @media only screen and (min-width: 768px) {
+        .storiesGridCard.card${cardInfo.indexOf(card)} {
+            background-image: url("https://res.cloudinary.com/djuu2phoy/image/upload/v1628528976/assets/stories/desktop/archDesktopDark_vumzzg.jpg");
+        }
+    }
+    </style>
+  <div class="storiesGridCard card${cardInfo.indexOf(card)}" >
     <h3>${card.date}</h3>
     <h4>by ${card.author}</h4>
     <hr />
