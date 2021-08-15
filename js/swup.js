117dad328808990b3cc37821697b4480e746fc42
Scott Parker <scottparkerdesign@gmail.com>
2021-08-15 10:11:40 -0400

    begin styling pricing page, temporarily remove swup animations

--- js/swup.js
+++ js/swup.js
@@ -1,3 +1,9 @@
 /* eslint-disable */
 
 const swup = new Swup();
+
+function resetNav() {
+    document.querySelector(".menu-icon__checkbox").checked = false;
+}
+
+swup.on('contentReplaced', resetNav);
