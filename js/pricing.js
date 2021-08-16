3bf3f15728e224da62d328ec244a1dad8ec3db43
Scott Parker <scottparkerdesign@gmail.com>
2021-08-16 15:00:04 -0400

    add updatePrice function to pricing.js

--- js/pricing.js
+++ js/pricing.js
@@ -5,19 +5,23 @@ const basic = document.querySelectorAll('.basic');
 const pro = document.querySelectorAll('.pro');
 const business = document.querySelectorAll('.business');
 
+function updatePrice(card, price) {
+  card.forEach((item) => (item.innerText = price));
+}
+
 function togglePrice() {
   if (slider.classList.contains('sliderActive')) {
     monthly.classList.remove('greyed');
     yearly.classList.add('greyed');
-    basic.forEach((item) => (item.innerText = '$19.00'));
-    pro.forEach((item) => (item.innerText = '$39.00'));
-    business.forEach((item) => (item.innerText = '$99.00'));
+    updatePrice(basic, '$19.00');
+    updatePrice(pro, '$39.00');
+    updatePrice(business, '$99.00');
   } else {
     monthly.classList.add('greyed');
     yearly.classList.remove('greyed');
-    basic.forEach((item) => (item.innerText = '$190.00'));
-    pro.forEach((item) => (item.innerText = '$390.00'));
-    business.forEach((item) => (item.innerText = '$990.00'));
+    updatePrice(basic, '$190.00');
+    updatePrice(pro, '$390.00');
+    updatePrice(business, '$990.00');
   }
   slider.classList.toggle('sliderActive');
 }
