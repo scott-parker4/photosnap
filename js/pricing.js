92c8cb1f2433ae092d9e4a6b5d1d3ea887a533d0
Scott Parker <scottparkerdesign@gmail.com>
2021-08-16 14:51:04 -0400

    edit pricing.js to include all pricing cards

--- js/pricing.js
+++ js/pricing.js
@@ -2,22 +2,22 @@ const slider = document.querySelector('label');
 const monthly = document.querySelector('.monthly');
 const yearly = document.querySelector('.yearly');
 const basic = document.querySelectorAll('.basic');
-const pro = document.querySelector('.pro');
-const business = document.querySelector('.business');
-console.log(basic);
+const pro = document.querySelectorAll('.pro');
+const business = document.querySelectorAll('.business');
+
 function togglePrice() {
   if (slider.classList.contains('sliderActive')) {
     monthly.classList.remove('greyed');
     yearly.classList.add('greyed');
     basic.forEach((item) => (item.innerText = '$19.00'));
-    pro.innerText = '$39.00';
-    business.innerText = '$99.00';
+    pro.forEach((item) => (item.innerText = '$39.00'));
+    business.forEach((item) => (item.innerText = '$99.00'));
   } else {
     monthly.classList.add('greyed');
     yearly.classList.remove('greyed');
     basic.forEach((item) => (item.innerText = '$190.00'));
-    pro.innerText = '$390.00';
-    business.innerText = '$990.00';
+    pro.forEach((item) => (item.innerText = '$390.00'));
+    business.forEach((item) => (item.innerText = '$990.00'));
   }
   slider.classList.toggle('sliderActive');
 }
