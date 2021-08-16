b030d6a9d683ae98be134bdcd23db1625e40cbac
Scott Parker <scottparkerdesign@gmail.com>
2021-08-16 13:01:49 -0400

    style basic pricing card for tablet

--- js/pricing.js
+++ js/pricing.js
@@ -1,21 +1,21 @@
 const slider = document.querySelector('label');
 const monthly = document.querySelector('.monthly');
 const yearly = document.querySelector('.yearly');
-const basic = document.querySelector('.basic');
+const basic = document.querySelectorAll('.basic');
 const pro = document.querySelector('.pro');
 const business = document.querySelector('.business');
-
+console.log(basic);
 function togglePrice() {
   if (slider.classList.contains('sliderActive')) {
     monthly.classList.remove('greyed');
     yearly.classList.add('greyed');
-    basic.innerText = '$19.00';
+    basic.forEach((item) => (item.innerText = '$19.00'));
     pro.innerText = '$39.00';
     business.innerText = '$99.00';
   } else {
     monthly.classList.add('greyed');
     yearly.classList.remove('greyed');
-    basic.innerText = '$190.00';
+    basic.forEach((item) => (item.innerText = '$190.00'));
     pro.innerText = '$390.00';
     business.innerText = '$990.00';
   }
